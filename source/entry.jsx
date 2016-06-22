4df71019f3ddb2b2d3e4ebbd7d1f2f72acc3cedb
Hyunkyu Yoon <hkyoon85@gmail.com>
2016-06-22 16:39:57 +0900

    Improve example code

--- source/entry.jsx
+++ source/entry.jsx
@@ -1,9 +1,11 @@
 import './entry.scss';
 import React, { PropTypes } from 'react';
 import ReactDOM from 'react-dom';
+import { Iterable, List } from 'immutable';
 import _ from 'underscore';
 
-const labNamesBucket = [
+// original data is an immutable array (List)
+const labNamesOriginalBucket = List([
   '오*석', '강*경', '유*재', '이*선', '안*수', '정*영',
   '권*철', '김*열', '이*혜', '이*훈', '이*기', '오*철', '채*영',
   '이*은', '이*수', '배*섭', '양*승', '윤*규', '이*민',
@@ -11,7 +13,9 @@ const labNamesBucket = [
   '권*희', '신*연', '권*',
   '이*', '이*정', '안*희', '문*열',
   '김*설', '은*현', '이*호', '박*현', '최*준',
-];
+]);
+
+const aa;
 
 const associateRandomNumber = (range) =>
   (name) => (
@@ -28,23 +32,34 @@ const leaveOnlyField = (field) =>
     }
   );
 
-const objectWithSingleFieldToArray = (obj) => (
-  obj[_.keys(obj)[0]]
-);
+const objectWithSingleFieldToValue = (obj) => 
+  obj[Object.keys(obj)[0]]
 
-const shuffleLabNamesBucket = (array) =>
-  _.chain(array)
+const returnFieldValue = (field) =>
+  (obj) => (
+    obj[field]
+  )
+
+const shuffleLabNamesBucket = (iterable) =>
+  iterable
     .map(associateRandomNumber(100))
-    .sortBy((obj) => obj.number)
+    .sortBy(returnFieldValue('number'))
     .map(leaveOnlyField('name'))
-    .map(objectWithSingleFieldToArray)
-    .value();
+    .map(objectWithSingleFieldToValue)
+
+// custom PropType checker
+const iterablePropTypeChecker = (props, propName, componentName) => {
+  if (!Iterable.isIterable(props[propName])) {
+    return new Error(`${componentName} received a non-iterable bucket`);
+  }
+  return null;
+}
 
 const GroupByTwo = (props) => (
   <div className="group-by-two">
     {
-      props.bucket.map(name =>
-        <div className="group-by-two__column">
+      props.bucket.map((name, index) =>
+        <div key={index} className="group-by-two__column">
           <div className="group-by-two__item">
             {name}
           </div>
@@ -55,25 +70,29 @@ const GroupByTwo = (props) => (
 );
 
 GroupByTwo.propTypes = {
-  bucket: PropTypes.array,
+  bucket: iterablePropTypeChecker,
 };
 
 class App extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      someState: null,
+      initialBucket: this.props.initialBucket,
     };
   }
   render() {
     return (
       <div>
         <GroupByTwo
-          bucket={shuffleLabNamesBucket(labNamesBucket)}
+          bucket={shuffleLabNamesBucket(this.props.initialBucket)}
         />
       </div>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+App.propTypes = {
+  initialBucket: iterablePropTypeChecker,
+};
+
+ReactDOM.render(<App initialBucket={labNamesOriginalBucket} />, document.getElementById('app'));
